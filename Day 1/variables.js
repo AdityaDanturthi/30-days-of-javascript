b762b7645454454722e088fda80f1dba174a652b
Aditya Danturthi <aditya2096@gmail.com>
2023-01-04 14:02:08 -0330

    Create variables.js

---
+++ Day 1/variables.js
@@ -0,0 +1,12 @@
+// Declaring different variables of different data types
+
+let name = 'Aditya'
+let age = 100
+
+const gravity = 9.81
+const PI = 3.14
+
+//Declaring variables in a single line separated by comma
+
+let name = 'Aditya',
+age = 100
