b61634886d419ce56b7538bfc5da55326bd30ea5
Aditya Danturthi <aditya2096@gmail.com>
2023-01-10 08:22:20 -0330

    Create primitiveDataTypes.js

---
+++ Day 2/primitiveDataTypes.js
@@ -0,0 +1,16 @@
+let word = 'JavaScript'
+
+let numOne = 1
+let numTwo = 1
+
+console.log(numOne == numTwo)
+
+let js = 'JavaScript'
+let py = 'Python'
+
+console.log(js == py)
+
+let lightOn = true
+let lightOff = false
+
+console.log(lightOn == lightOff)
