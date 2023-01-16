6078382462141b811f1e40d050f5f96cdf31b792
Aditya Danturthi <aditya2096@gmail.com>
2023-01-15 22:08:14 -0330

    Update strings.js

--- Day 2/strings.js
+++ Day 2/strings.js
@@ -3,3 +3,7 @@ let space = '' //emoty string
 let name = 'Aditya'
 let language = "JavaScript"
 console.log(name+' '+`is proficient in`+' '+language+'!')
+
+// String concatenation
+let strconcat = name+' '+`is proficient in`+' '+language+'!'
+console.log(strconcat)
