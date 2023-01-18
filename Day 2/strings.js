c4a0007ac15e92dc8f478b531357f47e302d1dfb
Aditya Danturthi <aditya2096@gmail.com>
2023-01-18 09:33:34 -0330

    Update strings.js

--- Day 2/strings.js
+++ Day 2/strings.js
@@ -7,3 +7,21 @@ console.log(name+' '+`is proficient in`+' '+language+'!')
 // String concatenation
 let strconcat = name+' '+`is proficient in`+' '+language+'!'
 console.log(strconcat)
+
+// String Methods
+console.log('Length of name:',name.length)
+
+firstLetter = name[0]
+console.log('First letter of name:',firstLetter)
+
+lastIndex = name.length-1
+console.log('Last letter of name:',name[lastIndex])
+
+upperName = name.toUpperCase()
+console.log('Name in uppercase:',upperName)
+
+lowerName = name.toLowerCase()
+console.log('Name in lowercase:',lowerName)
+
+console.log('Substr of name:',name.substr(0,3)) //substr(): It takes two arguments, the starting index and number of characters to slice.
+console.log('Substring of name:',name.substring(0,3)) //substring(): It takes two arguments, the starting index and the stopping index but it doesn't include the character at the stopping index.
