7d382ebd9b15792ba3be0b8ebfee78564e109666
Aditya Danturthi <aditya2096@gmail.com>
2023-01-18 13:34:57 -0330

    typeof and typecasting

---
+++ Day 2/CheckingandChangingDataTypes.js
@@ -0,0 +1,18 @@
+// Checking Data Types
+let name = 'Aditya'
+let age = 260
+const gravity = 9.81
+
+console.log("Data type of name:",typeof name)
+console.log("Data type of age:",typeof age)
+console.log("Data type of gravity:",typeof gravity)
+
+// Type Casting
+let str10 = '10'
+console.log("Type of str10:",typeof str10)
+let num10 = parseInt(str10) //str to int
+console.log("Type of num10:",typeof num10)
+
+console.log("gravity:",gravity)
+let gravityInt = parseInt(gravity) //float to int
+console.log("gravityInt:",gravityInt)
