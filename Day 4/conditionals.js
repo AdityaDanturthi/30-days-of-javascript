bc41194dfe66019684d1071d86902e383f92904e
Aditya Danturthi <aditya2096@gmail.com>
2023-01-21 22:32:30 -0330

    switch case

--- Day 4/conditionals.js
+++ Day 4/conditionals.js
@@ -24,3 +24,28 @@ if(num > 0) {
 } else {
     console.log(`${num} is zero!`)
 }
+
+// Switch case
+let num1 = parseInt(prompt('Please first number:'))
+let num2 = parseInt(prompt('Please second number:'))
+let selection = parseInt(prompt('Please select from one of the following options:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n'))
+
+switch(selection){
+    case 1:
+        console.log(`Addition = ${num1+num2}`)
+        break
+    case 2:
+        console.log(`Subtraction = ${num1-num2}`)
+        break
+    case 3:
+        console.log(`Multiplication = ${num1*num2}`)
+        break
+    case 4:
+        if(num2 > 0){
+        console.log(`Division = ${num1/num2}`)
+        } else {
+            console.log('Division is not possible because the denominator is 0!')
+        }
+        break
+    default:
+        console.log('No a valid selection. Please try again!')
