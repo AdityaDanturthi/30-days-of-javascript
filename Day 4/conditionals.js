7801bf21c59ae1eb8c05ea4a8bdc951ce7164d0f
Aditya Danturthi <aditya2096@gmail.com>
2023-01-21 22:09:13 -0330

    if else if else

--- Day 4/conditionals.js
+++ Day 4/conditionals.js
@@ -13,3 +13,14 @@ if(number > 0){
 } else {
     console.log(`${number} is negative!`)
 }
+
+// if else if else
+let num = 0 
+
+if(num > 0) {
+    console.log(`${num} is positive!`)
+} else if(num < 0) {
+    console.log(`${num} is negative!`)
+} else {
+    console.log(`${num} is zero!`)
+}
