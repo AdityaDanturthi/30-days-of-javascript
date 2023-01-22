edeb4c3c9a1797331a82aff11efa4d9d4067b557
Aditya Danturthi <aditya2096@gmail.com>
2023-01-21 22:03:28 -0330

    if and if else

---
+++ Day 4/conditionals.js
@@ -0,0 +1,15 @@
+// Conditionals
+let num = 1
+
+// if
+if(num == 1){
+    console.log(`Num = ${num}!`)
+}
+
+// if else
+let number = -1
+if(number > 0){
+    console.log(`${number} is positive!`)
+} else {
+    console.log(`${number} is negative!`)
+}
