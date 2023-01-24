cddc3dbcf9e0a05fd3bfccc64eaccd652c944ab4
Aditya Danturthi <aditya2096@gmail.com>
2023-01-24 15:51:41 -0330

    creating an array

---
+++ Arrays.js
@@ -0,0 +1,7 @@
+// Creating an array
+
+const arr = Array() // Creating an empty array using an array constructor
+console.log(arr)
+
+const arr1 = [] // Creating an empty array using an square brackets
+console.log(arr1)
