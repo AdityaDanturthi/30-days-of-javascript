733204872c844e7c0023d92ac577bef5ab53cbda
Aditya Danturthi <aditya2096@gmail.com>
2023-01-26 16:10:04 -0330

    creating an array using split and modifying an array

--- Day 5/Arrays.js
+++ Day 5/Arrays.js
@@ -14,3 +14,24 @@ console.log('Length of skills:', skills.length)
 
 let arr1 = ['Aditya', 25, {skills: ['Python', 'JS', 'HTML', 'CSS', 'SQL', 'Power BI', 'Tableau']}] // array containing different data types
 console.log(arr1)
+
+// Creating an array is using split
+let js = 'JavaScript'
+const charsinJS = js.split('')
+console.log('Characters in JS:', charsinJS)
+
+let companieslst = 'Amazon, Google, Facebook, Apple'
+const ITCompanies = companieslst.split(',')
+console.log('Companies:', ITCompanies)
+
+// Accessing elements in an array by index
+fruits = ['Apple', 'Orange', ' Pineapple', 'Watermelon']
+console.log('First fruit in the list:',fruits[0])
+
+lastIndex = fruits.length - 1
+lastFruit = fruits[lastIndex]
+console.log('First fruit in the list:', lastFruit)
+
+// Modifying an array 
+fruits[1] = 'Mango'
+console.log(fruits[1])
