237090b4e649481cc5200bbfdf050772e205cb50
AdityaDanturthi <aditya2096@gmail.com>
2023-04-22 19:17:01 -0230

    anonymous function using arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,8 @@
+// Anonymous function
+const magic = function() {
+    return new Date();
+};
+
+// Anonymous function converted into an arrow function (variable name is different because they are const)
+
+const magic1 = () => new Date();
