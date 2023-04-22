ea347b4bead9215d70815f52769f60d043181fa2
AdityaDanturthi <aditya2096@gmail.com>
2023-04-22 18:54:02 -0230

    multiple conditional (ternary) operators

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -1,5 +1,13 @@
+// Ternary operator
 function checkEqual(a, b) {
     return a===b ? true : false;
 }
 
 console.log(checkEqual(1,1));
+
+// Multiple conditional (ternary) operators
+function checkSign(num) {
+    return num > 0 ? "Positive" : num < 0 ? "Negative" : "zero";
+}
+
+console.log(checkSign(2));
