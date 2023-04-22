ccc2eb50a65c33da8f103d2f6b09c789c185a499
AdityaDanturthi <aditya2096@gmail.com>
2023-04-22 19:24:33 -0230

    converting an anonymous function into an arrow function

--- arrowFunction.js
+++ arrowFunction.js
@@ -6,3 +6,14 @@ const magic = function() {
 // Anonymous function converted into an arrow function (variable name is different because they are const)
 
 const magic1 = () => new Date();
+
+// Arrow function with parameters
+var myConcat = function(arr1, arr2) {
+    return arr1.concat(arr2);
+};
+
+console.log(myConcat([1,2], [3,4,5]));
+
+// Conveting the above function into an arrow function
+const myConcat1 = (a1, a2) => a1.concat(a2);
+console.log(myConcat1(JSON.stringify([1,2,3,4,5], [6,7,8,9,10])));
