a48d1f2108848ec12d73dc2bfaca5191cdbad468
AdityaDanturthi <aditya2096@gmail.com>
2023-04-22 11:30:42 -0230

    accessing object properties using dot notation

--- JSObjects.js
+++ JSObjects.js
@@ -5,3 +5,10 @@ var dog = {
     "tail": 1,
     "friends": ["dog1", "dog2", 'dog3', 'dog4']
 };
+
+// Accessing a items in JSON
+var nameValue = dog.name;
+var friendsValue = dog.friends;
+
+console.log(nameValue);
+console.log(friendsValue);
