a0c3aedd702bdd0e24fcc2039bb6a68315cddbbb
AdityaDanturthi <aditya2096@gmail.com>
2023-04-22 15:14:01 -0230

    for and while loops

---
+++ Loops.js
@@ -0,0 +1,30 @@
+// Iterate with while loop
+var myArray = [];
+
+var i = 0;
+
+while(i < 5) {
+    myArray.push(i);
+    i++;
+}
+
+i= 0; 
+
+console.log(myArray)
+
+while(i < 5) {
+    console.log(myArray[i]);
+    i++;
+}
+
+// Iterate with For loop
+var myArray1 = [];
+for(j=0; j < 5; j++) {
+    myArray1.push(j);
+}
+
+console.log(myArray1);
+
+for(j=0; j < 5; j++) {
+    console.log(myArray1[j]);
+}
