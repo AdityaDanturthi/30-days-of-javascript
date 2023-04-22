49a666b7d1960acb3ae1e5884dfc80c274ff129a
AdityaDanturthi <aditya2096@gmail.com>
2023-04-22 15:16:42 -0230

    even array

--- Loops.js
+++ Loops.js
@@ -19,12 +19,19 @@ while(i < 5) {
 
 // Iterate with For loop
 var myArray1 = [];
-for(j=0; j < 5; j++) {
+for(var j=0; j < 5; j++) {
     myArray1.push(j);
 }
 
 console.log(myArray1);
 
-for(j=0; j < 5; j++) {
+for(var j=0; j < 5; j++) {
     console.log(myArray1[j]);
 }
+
+var evenArray = [];
+for( var e=0; e<11; e += 2) {
+    evenArray.push(e);
+}
+
+console.log("Even array:",evenArray);
