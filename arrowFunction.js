262b59f079ecb8e0d92b2aed85b9afd53fa5c308
AdityaDanturthi <aditya2096@gmail.com>
2023-04-22 21:00:56 -0230

    higher order arrow functions

--- arrowFunction.js
+++ arrowFunction.js
@@ -17,3 +17,8 @@ console.log(myConcat([1,2], [3,4,5]));
 // Conveting the above function into an arrow function
 const myConcat1 = (a1, a2) => a1.concat(a2);
 console.log(myConcat1(JSON.stringify([1,2,3,4,5], [6,7,8,9,10])));
+
+// Higher order arrow functions (val = 1 sets the default value of val to 1 incase it doesn't get passed in the function)
+const increment = (num, val = 1) => num + val
+console.log("5+2:", increment(5,2));
+console.log("5+1:", increment(5));
