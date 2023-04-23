6b25a660ebfdbbdb2947af721572bf54f49308b4
AdityaDanturthi <aditya2096@gmail.com>
2023-04-23 14:07:04 -0230

    class example

--- classandConstructor.js
+++ classandConstructor.js
@@ -1,8 +1,18 @@
 class spaceShuttle  {
-    conntructor (targetPlanet) {
+    constructor (targetPlanet) {
         this.targetPlanet = targetPlanet;
     }
 }
 
 let planet = new spaceShuttle('Mars');
 console.log(planet.targetPlanet);
+
+// example 1
+class vegetable {
+    constructor(name1){
+        this.name1 = name1;
+    }
+}
+
+let carrot = new vegetable('Carrot');
+console.log(carrot.name1);
