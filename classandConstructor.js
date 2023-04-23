3191d67f28c50547d832081a10c5692aac994bd1
AdityaDanturthi <aditya2096@gmail.com>
2023-04-23 14:49:34 -0230

    getter and setter example

--- classandConstructor.js
+++ classandConstructor.js
@@ -16,3 +16,42 @@ class vegetable {
 
 let carrot = new vegetable('Carrot');
 console.log(carrot.name1);
+
+// Getter and Setter
+class Book {
+    constructor(author){
+        this.author = author;
+    }
+    //getter
+    get writer(){
+        return this._author; // _ before a vairable means it is private variable within the scope of where it was declared
+    }
+
+    //setter
+    set writer(updatedAuthor){
+        this._author = updatedAuthor;
+    }
+}
+
+// Getter and Setter example
+class thermostat{
+    constructor (temp){
+        this._temp = 5/9 * (temp - 32);
+    }
+    //getter 
+    get temperature(){
+        return this._temp;
+    }
+    //setter
+    set temperature(updatedTemp){
+        this._temp = updatedTemp;
+    }
+}
+
+let thermos = new thermostat(25);
+let temp = thermos.temperature;
+console.log("Initial temperature:", temp)
+thermos.temperature = 75;
+updatedTemp = thermos.temperature
+console.log("Updated temperature:", updatedTemp);
+
