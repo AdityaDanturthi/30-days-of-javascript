5ed336644f92036f65f976c226b0de9a792e1114
Aditya Danturthi <aditya2096@gmail.com>
2023-04-29 16:23:49 -0230

    addEventListener()

---
+++ DOM/Event Listeners/app.js
@@ -0,0 +1,8 @@
+// DOM Mamipulation
+
+// Event Listeners
+let buttonTwo = document.querySelector(".btn-2");
+function alertBtn() {
+    alert('I love JavaScript too!');
+}
+buttonTwo.addEventListener("click", alertBtn);
